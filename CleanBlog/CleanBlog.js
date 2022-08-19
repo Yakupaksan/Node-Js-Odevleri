5e0e7f8d678fd4eb145886e762a0b95a5638ed82
Yakupaksan <yakupaksann97@gmail.com>
2022-08-19 13:41:52 +0300

    cleanblog-2.odev

--- CleanBlog/CleanBlog.js
+++ CleanBlog/CleanBlog.js
@@ -1,13 +1,30 @@
 const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const app = express();
+app.use(express.static('public'))
 
-const app = express()
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs')
+
+
+//MIDDLEWARES
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    res.render('index')
 })
 
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
 
 
 const post = 3000;

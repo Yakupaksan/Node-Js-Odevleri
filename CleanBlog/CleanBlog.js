8e79044839a1ffec0a62b57f29d12e547bc32d17
Yakupaksan <yakupaksann97@gmail.com>
2022-08-23 10:05:01 +0300

    clean 5

--- CleanBlog/CleanBlog.js
+++ CleanBlog/CleanBlog.js
@@ -1,63 +1,54 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const path = require('path')
-const ejs = require('ejs')
+const path = require("path");
+const ejs = require("ejs");
 const app = express();
-app.use(express.static('public'))
+const methodOverride = require("method-override");
+const pageController = require("./controllers/pageController");
+const postController = require("./controllers/postController");
+const AddPost = require("./models/AddPost");
 
-const AddPost = require('./models/AddPost'); 
-
-
-
-mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-
-  
 //TEMPLATE ENGINE
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 //MIDDLEWARES
-app.get('/', async (req, res) => {
-    const addposts = await AddPost.find({}).sort('-dateCreated')
-    res.render('index', {
-        addposts})
-    });
+app.get("/", postController.getAllPosts);
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
+app.get("/about", pageController.getAboutPage);
 
-app.get('/add_post', (req, res) => {
-    res.render('add_post')
-})
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
 
-app.get('/post', (req, res) => {
-    res.render('post')
-}) 
+app.get("/post", (req, res) => {
+  res.render("post");
+});
 
+app.post("/AddPost", pageController.getAddPage);
 
-app.post('/AddPost', async (req, res) => {
-    await AddPost.create(req.body)            
-    res.redirect('/')
-  });
+app.get("/addposts/:id", postController.getPost);
 
-  app.get("/addposts/:id", async (req, res) => {
-    const addposts = await AddPost.findById(req.params.id)
-    res.render('post', {
-        addposts
-    })
-  });
- 
+app.get("/addposts/edit/:id", pageController.getEditPage);
 
+app.put("/addposts/:id", postController.getUpdate);
+
+app.delete("/addposts/:id", postController.getDelete);
 
 const post = 3000;
 app.listen(post, () => {
-    console.log(`${post} numarali post basarili bir sekilde acildi. `)
-})
+  console.log(`${post} numarali post basarili bir sekilde acildi. `);
+});
